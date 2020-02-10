cd98db1c57d855195961976859e9076d94614daa
Steve Frenzel <stevefrenzel@gmx.de>
2020-02-10 13:45:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// COMPONENTS
+import BottomAppBar from './components/BottomAppBar';
+import Resources from './components/Resources';
+import DataFeedback from './components/DataFeedback';
+
+// MATERIAL UI - CORE
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <DataFeedback />
+      <BottomAppBar />
+    </>
   );
-}
+};
 
 export default App;
