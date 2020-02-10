1f7f67b58266e18f534b1acee8ce8320b536e1a4
Steve Frenzel <stevefrenzel@gmx.de>
2020-02-10 13:46:00 +0100

    Create Resources.jsx

---
+++ src/components/Resources.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// MATERIAL UI - CORE
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+// MATERIAL UI - ICONS
+
+const useStyles = makeStyles(theme => ({}));
+
+const Resources = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Typography>RESOURCES</Typography>
+    </>
+  );
+};
+
+export default Resources;
