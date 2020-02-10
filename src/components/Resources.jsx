a53f70223b4d49febe28f0540d788a690f9dec80
Steve Frenzel <stevefrenzel@gmx.de>
2020-02-10 16:09:16 +0100

    Update Resources.jsx

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
 // MATERIAL UI - CORE
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 // MATERIAL UI - ICONS
 
-const useStyles = makeStyles(theme => ({}));
+// const useStyles = makeStyles(theme => ({}));
 
-const Resources = () => {
-  const classes = useStyles();
+const Resources = ({ userInfo }) => {
+  // const classes = useStyles();
+
+  // DISPLAY THIS DATA:
+  // the EOS token balance √
+  // the value in USD
+  // the staked resources for CPU and NET as well as the current consumption
+  // the RAM consumption √
 
   return (
     <>
-      <Typography>RESOURCES</Typography>
+      <Typography>EOS token balance: {userInfo.core_liquid_balance}</Typography>
+      <Typography>EOS token balance in USD: ?</Typography>
+      <Typography>RAM usage: {userInfo.ram_usage}</Typography>
     </>
   );
 };
