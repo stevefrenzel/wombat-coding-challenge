4db3b120241576f37211b1594791056085013b28
Steve Frenzel <stevefrenzel@gmx.de>
2020-02-10 16:09:12 +0100

    Update DataFeedback.jsx

--- src/components/DataFeedback.jsx
+++ src/components/DataFeedback.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 // MATERIAL UI - CORE
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 // MATERIAL UI - ICONS
 
-const useStyles = makeStyles(theme => ({}));
+// const useStyles = makeStyles(theme => ({}));
 
 const Resources = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
     <>
