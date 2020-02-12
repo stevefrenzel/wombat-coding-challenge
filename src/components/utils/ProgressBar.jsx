e606bd291b13e6136ac5e76561fc3f86590086f6
Steve Frenzel <stevefrenzel@gmx.de>
2020-02-12 09:59:04 +0100

    Create ProgressBar.jsx

---
+++ src/components/utils/ProgressBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// MATERIAL UI - CORE
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const Progress = ({ ...props }) => {
+  return <LinearProgress {...props} />;
+};
+
+export default Progress;
