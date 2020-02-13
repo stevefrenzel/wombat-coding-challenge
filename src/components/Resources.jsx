d9ac11a0f42a3283b34d3279a6299022c75b5c26
Steve Frenzel <stevefrenzel@gmx.de>
2020-02-13 12:10:15 +0100

    Update Resources.jsx

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -49,9 +49,6 @@ const Resources = ({ userInfo }) => {
           <Typography content='Resources' variant='h6' />
           <Divider className={classes.divider} />
         </Grid>
-        <Grid item xs={12}>
-          <Typography content={`Staked Total:`} variant='body2' align='left' />
-        </Grid>
         <Grid item xs={12} className={classes.dataFeedback}>
           <DataFeedback
             showStaked
